5564fbd66012563ee09ca783bb6d90c212a7286f
sprin <sprin>
2014-02-05 11:01:51 -0800

    Update setup/readme

--- README.txt
+++ README.txt
@@ -1,5 +1,9 @@
+==============
+nose-easy-init
+==============
+
 A nose plugin, enabled by default, which imports an initialization script given
-by an environment variable. To use, set `NOSE_INIT_PATH` to the absolute path
+by an environment variable. To use, set ``NOSE_INIT_PATH`` to the absolute path
 of the script you would like to import. This script will be imported before
 nose collects or runs any tests.
 
--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='1.0',
     packages=['nose_easy_init',],
     description='A plugin for nose to easily specify an initialization  script',
-    author='sprin',
     url = 'https://github.com/sprin/nose-easy-init',
+    author = 'sprin',
+    author_email = 'sprin.pypi@fastmail.net',
     download_url = 'https://github.com/sprin/nose-easy-init/tarball/1.0',
     entry_points = {
         'nose.plugins.0.10': [
